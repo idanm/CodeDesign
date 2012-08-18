4878caa84ce4aa174e479bc5afb0ecaec57fef7c
Idan Mitrofanov <idan1986@gmail.com>
2012-08-18 16:39:54 +0300

    rename for the future.

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 	<!-- Page -->
 	<section id="page" class="container">
 		<header class="clearfix">
-			<?php echo CodeDesign::Content('markdown/header'); ?>
+			<?php echo CodeDesign::Content('markdown/default/header'); ?>
 			
 			<div class="btn-group">
 				<a class="btn btn-success btn-large dropdown-toggle" data-toggle="dropdown" href="#">
@@ -40,14 +40,14 @@
 		</header>
 		
 		<article class="well">
-			<?php echo CodeDesign::Content('markdown/content'); ?>
+			<?php echo CodeDesign::Content('markdown/default/content'); ?>
 			
 			<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://bit.ly/PvVSB7" data-counturl="http://bit.ly/PvVSB7" data-lang="en" data-count="vertical">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 		</article>
 			
 		<footer>
-			<?php echo CodeDesign::Content('markdown/footer'); ?>
+			<?php echo CodeDesign::Content('markdown/default/footer'); ?>
 		</footer>
 	</section>
 	
