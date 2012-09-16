de286c27c80273e8bbcdefbd64c0e33b9b6661d5
Idan Mitrofanov <idan1986@gmail.com>
2012-09-16 21:08:39 +0300

    backup got modimodified

--- config/backup.example.sh
+++ config/backup.example.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-TO=/some/where/you/want/to/store/$(date +%Y%m%d%H%M)_backup.tar.gz
+FILE=/some/where/you/want/to/store/$(date +%Y%m%d).tar.gz
+DROPBOX="/Users/Name/Dropbox/Backup/"
+GOOGLE_DRIVE="/Users/Name/Google Drive/Backup/"
 BACKUP="what/you/want/to/backup"
 
   echo "off we go!"
-  tar czvf $TO --exclude='.DS_Store' $BACKUP
+  tar czvf $FILE --exclude='.DS_Store' $BACKUP
 
   if [ $? == 0 ]; then
-    echo "Backup was created."
+    echo "Backup created."
+    cp -rv $FILE "$DROPBOX"
+    cp -rv $FILE "$GOOGLE_DRIVE"
+    echo "Backup distributed."
+    rm -rf $FILE
+    echo "$FILE removed."
   else
     echo "Backup failed."
   fi
