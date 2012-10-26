d03f1d89b8b53b5df7e8bb166e63ecc524651787
Idan Mitrofanov <idan1986@gmail.com>
2012-10-27 01:58:23 +0200

    added environment.json exists check

--- php/environment-mount.php
+++ php/environment-mount.php
@@ -6,9 +6,14 @@
   // Error Handeling off.
   error_reporting(0);
 
-  // Version Check
+  // PHP Version Check
   if (!version_compare(PHP_VERSION, '5.3.0', '>')) {
-    die("Please update you server's php version to at least 5.3.0");
+    die("Please update you server's php version to at least 5.3.0.");
+  }
+
+  // Environment File Check
+  if (!file_exists(dirname(__FILE__) . '/../config/environment.json')) {
+    die("Please make a copy of environment.json file. an example file can be found at the config folder.");
   }
 
 ?>
